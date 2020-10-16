17a4649147f081a4b69b4b978086c5502ed3037c
York <59372141+Dermaed@users.noreply.github.com>
2020-10-16 18:08:35 +0300

    всё сделано

--- app/src/main/java/com/example/hihomosexualist/MainActivity.kt
+++ app/src/main/java/com/example/hihomosexualist/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.hihomosexualist
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mHelloTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mHelloTextView = findViewById(R.id.textView)
+
+        var imageButton: ImageButton = findViewById(R.id.imageButton)
+
+       // val imageButton = findViewById<ImageButton>(R.id.imageButton)
+
+        imageButton.setOnClickListener {
+            mHelloTextView.setText("Hello, Homosexualist!")
+
+        }
     }
 }
